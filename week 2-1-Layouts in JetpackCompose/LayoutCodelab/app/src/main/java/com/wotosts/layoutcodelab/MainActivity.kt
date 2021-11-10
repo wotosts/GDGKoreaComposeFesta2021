2fc887789e0789de74561ea644c4c2d94eb3b593
wotosts <tosts522@gmail.com>
2021-11-10 22:08:16 +0900

    week2 step1 ~ step5

---
+++ week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/wotosts/layoutcodelab/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.wotosts.layoutcodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.wotosts.layoutcodelab.ui.theme.LayoutCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    LayoutsCodelab()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Layout Codelab")
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) {
+        BodyContent(Modifier.padding(it))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Composable
+@Preview
+fun LayoutsCodelabPreview() {
+    LayoutCodelabTheme {
+        LayoutsCodelab()
+    }
+}
